8296599a3a054f5e4d4e024062b46f1a84e63d6d
oliver <oliver.poole.ruag@outlook.com>
2021-12-30 18:39:45 +0100

    slicing with text output working completely

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -75,19 +75,28 @@ def create_slice_inference_on_column(dat, model, column, cat_features):
     vals = dat[column].unique().tolist()
     encoder = load_encoder(os.path.normcase("starter/model/encoder"))
     lb = load_encoder(os.path.normcase("starter/model/lb"))
-
-    for i in vals:
-        dat_slice = dat.loc[dat[column] == i]
+    scores = np.zeros((len(vals), 3))
+    for i, val in enumerate(vals):
+        dat_slice = dat.loc[dat[column] == val]
         X_slice, y_slice, _, _ = process_data(
             dat_slice, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
         )
         preds_slice= inference(model, X_slice)
-        scores = compute_model_metrics(y_slice, preds_slice)
-        print(scores)
+        scores[i] = compute_model_metrics(y_slice, preds_slice)
+    
+    return list(zip(vals, scores))
+
+def write_slicing_resu(resu):
+    file = open("slice_output.txt","w")
+    for i in resu:
+        file.writelines(f"Category {i[0]} has precision: {i[1][0]}, recall: {i[1][1]}, fbeta: {i[1][2]}\n")
+    file.close()
+        
 
 def go():
     data = load_data(os.path.normcase("starter/data/census.csv"))
     data.columns = data.columns.str.replace(' ','')
+    data.replace(' ', '', regex=True)
     save_clean_data(data, os.path.normcase("starter/data/clean_census.csv"))
     
     cat_features = [
@@ -106,15 +115,16 @@ def go():
 
     model = train_model(X_train, y_train)
 
-    save_model(model, os.path.normcase("starter/model/RF_model"))
-    save_encoder(encoder, os.path.normcase("starter/model/encoder"))
-    save_encoder(lb, os.path.normcase("starter/model/lb"))
+    # save_model(model, os.path.normcase("starter/model/RF_model"))
+    # save_encoder(encoder, os.path.normcase("starter/model/encoder"))
+    # save_encoder(lb, os.path.normcase("starter/model/lb"))
     #model = load_model(os.path.normcase("starter/model/RF_model"))
     #encoder = load_encoder(os.path.normcase("starter/model/encoder"))
     #lb = load_encoder(os.path.normcase("starter/model/lb"))
 
     preds = inference(model, X_test)
     precision, recall, fbeta = compute_model_metrics(y_test, preds)
-    #create_slice_inference_on_column(data, model, "Education", cat_features)
+    resu = create_slice_inference_on_column(data, model, "education", cat_features)
+    write_slicing_resu(resu)
 
 go()
